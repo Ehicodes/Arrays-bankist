509618502b908c933fc2a29f9ebaf8c59e9d4a33
Ehicodes <158574212+Ehicodes@users.noreply.github.com>
2025-06-08 20:17:20 +0100

    FOREACH WITH MAPS AND SETS

--- starter/script.js
+++ starter/script.js
@@ -128,3 +128,25 @@ movements.forEach(function(mov, i, arr) {
     console.log(`Movement ${i + 1} You withdrew ${Math.abs(mov)}`)
   }
 })
+
+
+//FOREACH WITH MAPS AND SETS
+console.log('------FOREACH WITH MAPS-------------')
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+currencies.forEach(function(value, key, map) {
+   console.log(`${key}: ${value}`)
+})
+
+
+console.log('------FOREACH WITH SET-------------')
+const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR'])
+console.log(currenciesUnique)
+
+currenciesUnique.forEach(function(value, _, set) {
+ console.log(`${value}`)
+})
