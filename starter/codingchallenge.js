b5bc7e311859044a08f68c9398fc8bf06bfb8fb0
Ehicodes <158574212+Ehicodes@users.noreply.github.com>
2025-06-10 03:36:27 +0100

    codingchallenge 1. task 2

--- starter/codingchallenge.js
+++ starter/codingchallenge.js
@@ -3,13 +3,10 @@
 Working With Arrays 
 Coding Challenge #1 
 Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners about their dog's age, and stored the data into an array (one array for each). For now, they are just interested in knowing whether a dog is an adult or a puppy. 
-A dog is an adult if it is at least 3 years old, and it's a puppy if it's less than 3 years 
-old. 
+A dog is an adult if it is at least 3 years old, and it's a puppy if it's less than 3 years old. 
 Your tasks: 
-Create a function 'checkDogs', which accepts 2 arrays of dog's ages 
-('dogsJulia' and 'dogsKate'), and does the following things: 
-1. Julia found out that the owners of the first and the last two dogs actually have 
-cats, not dogs! So create a shallow copy of Julia's array, and remove the cat ages from that copied array (because it's a bad practice to mutate function parameters) 
+Create a function 'checkDogs', which accepts 2 arrays of dog's ages ('dogsJulia' and 'dogsKate'), and does the following things: 
+1. Julia found out that the owners of the first and the last two dogs actually have cats, not dogs! So create a shallow copy of Julia's array, and remove the cat ages from that copied array (because it's a bad practice to mutate function parameters) 
 2. Create an array with both Julia's (corrected) and Kate's data 
 3. For each remaining dog, log to the console whether it's an adult ("Dog number 1 
 is an adult, and is 5 years old") or a puppy ("Dog number 2 is still a puppy 
@@ -28,10 +25,13 @@ GOOD LUCK
 
 const checkDogs = function (dogsJulia, dogsKate) {
   //1
-  const juliaShallowCopy = dogsJulia.slice();
-  juliaShallowCopy.splice(0, 1);
-  juliaShallowCopy.splice(-2);
-  console.log(juliaShallowCopy);
+  const juliaCorrectedCopy = dogsJulia.slice();
+  juliaCorrectedCopy.splice(0, 1);
+  juliaCorrectedCopy.splice(-2);
+  console.log(juliaCorrectedCopy);
+  //2
+  const allDogsData = juliaCorrectedCopy.concat(dogsKate);
+  console.log(allDogsData);
 };
 
 checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
