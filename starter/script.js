5271d2251edbd589d9e38b009596c67bd7d1dd95
Ehicodes <158574212+Ehicodes@users.noreply.github.com>
2025-06-18 06:11:37 +0100

    every method

--- starter/script.js
+++ starter/script.js
@@ -435,3 +435,13 @@ console.log(movements.includes(-130));
 console.log(movements.some(mov => mov === -130));
 const anyDeposits = movements.some(mov => mov > 500);
 console.log(anyDeposits);
+
+//EVERY METHOD
+console.log(movements.every(mov => mov > 0));
+console.log(account4.movements.every(mov => mov > 0));
+
+//seperate callback
+const deposit = mov => mov > 0;
+console.log(movements.some(deposit));
+console.log(movements.every(deposit));
+console.log(movements.filter(deposit));
