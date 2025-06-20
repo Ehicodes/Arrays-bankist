b4e7b52fef882e7b462534c1d04f577051a35950
Ehicodes <158574212+Ehicodes@users.noreply.github.com>
2025-06-20 10:34:29 +0100

    flat and flatMap methods

--- starter/script.js
+++ starter/script.js
@@ -445,3 +445,23 @@ const deposit = mov => mov > 0;
 console.log(movements.some(deposit));
 console.log(movements.every(deposit));
 console.log(movements.filter(deposit));
+
+//FLAT AND FLATMAPS
+//flat
+const arr = [[1, 2, 3], [4, 5, 6], 7, 8];
+console.log(arr.flat());
+
+const arrDeep = [[[1, 2], 3], [4, [5, 6]], 7, 8];
+console.log(arrDeep.flat(2));
+
+const averageBalance = accounts
+  .map(arr => arr.movements)
+  .flat()
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(averageBalance);
+
+//flatMap
+const averageBalance2 = accounts
+  .flatMap(acc => acc.movements)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(averageBalance2);
