570f590801e94ce3eb4343dc5d094fb6631dda4f
Ehicodes <158574212+Ehicodes@users.noreply.github.com>
2025-06-22 19:36:40 +0100

    more ways of creating and filling arrays

--- starter/script.js
+++ starter/script.js
@@ -498,3 +498,37 @@ console.group(movements);
 //Descending
 movements.sort((a, b) => b - a);
 console.log(movements);
+
+//MORE WAYS OF CREATING AND FILLING ARRAYS
+const arr = [1, 2, 3, 4, 5, 6, 7];
+console.log(new Array(1, 2, 3, 4, 5, 6, 7));
+
+//Empty arrays + fill method
+const x = new Array(7);
+console.log(x);
+
+x.fill(1, 3, 5);
+x.fill(1);
+console.log(x);
+
+console.log(arr);
+
+arr.fill(23, 2, 6);
+
+console.log(arr);
+
+//Array.from
+const y = Array.from({ length: 7 }, () => 1);
+console.log(y);
+
+const z = Array.from({ length: 7 }, (_, i) => i + 1);
+console.log(z);
+
+labelBalance.addEventListener('click', function () {
+  const movementsUI = Array.from(
+    document.querySelectorAll('.movements__value'),
+    el => el.textContent.replace('€', '')
+  );
+
+  console.log(movementsUI);
+});
